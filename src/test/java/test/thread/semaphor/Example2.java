d02ae811facae932fc1955c03693caab17174d77
shibo <shibo@qiban.com>
2018-08-14 16:59:01 +0800

    多线程使用semaphore、countDownLatch 生产/消费者模式处理

---
+++ src/test/java/test/thread/semaphor/Example2.java
@@ -0,0 +1,38 @@
+package test.thread.semaphor;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Example2 {
+    public static AtomicInteger count = new AtomicInteger(0);
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // 用200个线程处理
+        Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal ; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        // 输出正常
+        System.out.println(count);
+    }
+    private static void add() {
+        count.incrementAndGet();
+    }
+}
