7c3b46527011c8d890938003cb609757cf107c97
shibo <shibo92@qq.com>
2018-08-28 23:26:01 +0800

    多线程生产者消费者Lock实现

--- src/test/java/test/thread/providerandconsumer/ProviderAndConsumer.java
+++ src/test/java/test/thread/providerandconsumer/ProviderAndConsumer.java
@@ -10,6 +10,7 @@ class Resource {
     private boolean hasChicken = false;
 
     public synchronized void set(String name) {
+        // 用while代替if，解决了线程获取执行权后，是否继续判断标记
         while (hasChicken) {
             try {
                 this.wait();
@@ -18,9 +19,10 @@ class Resource {
             }
         }
         this.name = name;
-        // this.count ++ ;
         System.out.println("生产者...生产---" + name + ++count);
         this.hasChicken = true;
+        // 这里用notify的话，t0执行完毕，唤醒另一个provider:t1，t1判断标记后休眠，造成所有线程都wait的局面，即死锁
+        // notifyAll解决了死锁问题
         this.notifyAll();
     }
 
