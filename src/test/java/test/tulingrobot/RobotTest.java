39444fbb92afdb450efb7513280a18b7790661dc
shibo <shibo@qiban.com>
2018-08-29 18:15:02 +0800

    图灵机器人demo

--- src/test/java/test/tulingrobot/RobotTest.java
+++ src/test/java/test/tulingrobot/RobotTest.java
@@ -1,34 +1,42 @@
 package test.tulingrobot;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class RobotTest {
-    // http://openapi.tuling123.com/openapi/api/v2
-    // 316330
-    // a12a64ce315149bbb5de1bd5a1553780
-
     public static void main(String[] args) {
-        String inputText = "你好";
-        String address = "http://openapi.tuling123.com/openapi/api/v2";
-        JSONObject param = getJsonParam(inputText);
-        String result = ConnectionUtil.sendPostForJson(address, param);
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        String apiAddress = "http://openapi.tuling123.com/openapi/api/v2";
+        String inputText = "";
+        while (!inputText.equals("exit")) {
+            inputText = scanner.nextLine();
+            JSONObject param = getJsonParam(inputText);
+            String result = ConnectionUtil.sendPostForJson(apiAddress, param);
+            String tulingResult = handleResult(result);
+            System.out.println(tulingResult);
+        }
+        scanner.close();
+        System.out.println("已退出");
     }
-    
-    private static JSONObject getJsonParam(String inputText){
+
+    // 解析json 获取返回的text
+    private static String handleResult(String result) {
+        return JSONObject.parseObject(result).getJSONArray("results")
+                .getJSONObject(0).getJSONObject("values")
+                .getString("text");
+    }
+
+    // 获取需要发送给图灵接口的json
+    private static JSONObject getJsonParam(String inputText) {
         String appKey = "a12a64ce315149bbb5de1bd5a1553780";
         String userId = "316330";
         String paramStr = "{" +
                 "\"reqType\":0," +
                 "    \"perception\": {" +
                 "        \"inputText\": {" +
-                "            \"text\": \""+ appKey +"\"" +
+                "            \"text\": \"" + inputText + "\"" +
                 "        }," +
                 "        \"inputImage\": {" +
                 "            \"url\": \"imageUrl\"" +
@@ -40,7 +48,7 @@ public class RobotTest {
                 "        \"userId\": \"" + userId + "\"" +
                 "    }" +
                 "}";
-        JSONObject param = JSONObject.parseObject(paramStr);
-        return param;
+        JSONObject resultJsonObj = JSONObject.parseObject(paramStr);
+        return resultJsonObj;
     }
 }
