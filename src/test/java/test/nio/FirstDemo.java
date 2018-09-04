9f517db2d99abb76328332359dbbdcb6de786cda
shibo <shibo@qiban.com>
2018-09-04 14:52:28 +0800

    nio demo

--- src/test/java/test/nio/FirstDemo.java
+++ src/test/java/test/nio/FirstDemo.java
@@ -16,11 +16,24 @@ public class FirstDemo {
             File outFile = new File("/Users/shibo/tmp/niotest_out.txt");
             FileOutputStream fout = new FileOutputStream(outFile);
             FileChannel fcOut = fout.getChannel();
-            fcIn.read(buffer);
+
+
             // clear() 方法重设缓冲区，使它可以接受读入的数据。
             // flip() 方法让缓冲区可以将新读入的数据写入另一个通道。
-            buffer.flip();
-            fcOut.write(buffer);
+            while (true) {
+                // 清空缓冲区
+                buffer.clear();
+                //从通道读取一个数据到缓冲区
+                int r = fcIn.read(buffer);
+                //判断是否有从通道读到数据
+                if (r == -1) {
+                    break;
+                }
+                //将buffer指针指向头部
+                buffer.flip();
+                //把缓冲区数据写入通道
+                fcOut.write(buffer);
+            }
 
             fcOut.close();
             fout.close();
