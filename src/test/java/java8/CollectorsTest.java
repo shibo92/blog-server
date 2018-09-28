d527975d1a3fc65dbc6df2d6b032c6542b9af4b2
shibo <shibo92@qq.com>
2018-09-28 10:09:12 +0800

    java8 demo

--- src/test/java/java8/CollectorsTest.java
+++ src/test/java/java8/CollectorsTest.java
@@ -2,14 +2,19 @@ package java8;
 
 import com.blog.entity.User;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 import java.sql.SQLOutput;
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CollectorsTest {
     @Test
-    public void Test1(){
+    public void testFuncation(){
         List<User> userList = new LinkedList<>();
         for (int i = 0; i <5; i++) {
             User u = new User();
@@ -48,8 +53,57 @@ public class CollectorsTest {
     }
 
     public static void sortByKey(List<User> userList){
-        // 将用户列表按删除和已删除分组
+        // 将用户列表按删除和未删除分组
         userList.sort((u1,u2) -> u2.getId().compareTo(u1.getId()));
     }
 
+    @Test
+    public void testReduce(){
+        int[] arr = {4,4,2};
+        // Arrays.stream(arr).reduce((x,y) -> x+y).ifPresent(System.out::println);
+        System.out.println(Arrays.stream(arr).reduce((x,y) -> x*y).orElse(0));;
+        System.out.println(Arrays.stream(arr).reduce(-1, (x, y) -> x + y));
+    }
+
+
+    @Test
+    public void testCount(){
+        String[] arr = {"4","4","2"};
+        IntPredicate lt10 = e -> e < 3;
+
+        System.out.println(Arrays.asList(arr).stream().mapToInt(Integer::valueOf).anyMatch(lt10));
+    }
+
+    @Test
+    public void testCollect(){
+        Integer[] arr = {4,4,2};
+        List<Integer> list = Arrays.stream(arr).collect(Collectors.toList());
+        System.out.println(list);
+    }
+
+    @Test
+    public void testCollect2(){
+        Integer[] arr = {4,3,2};
+        List<Integer> list = Arrays.stream(arr).collect(Collectors.toList());
+        Map<String,String> map = list.stream().collect(Collectors.toMap(String::valueOf, a -> String.valueOf(a*a)));
+        System.out.println(map);
+    }
+
+    @Test
+    public void testPeek() {
+        Integer[] arr = {4,3,2};
+        List<Integer> list = new LinkedList<>();
+        long count = Arrays.stream(arr).filter(x -> x > 2).peek(list::add).count();
+        System.out.println(list);
+        System.out.println(count);
+    }
+
+    @Test
+    public void testThen(){
+        Integer[] arr = {4,3,2};
+        Consumer<Integer> out = (x) -> System.out.println("out consume:"+x);
+        Consumer<Integer> err = (x) -> System.err.println("err consume:"+x);
+//        Arrays.stream(arr).forEach(out.andThen(err));
+        Arrays.stream(arr).forEach(err.andThen(out));
+    }
 }
