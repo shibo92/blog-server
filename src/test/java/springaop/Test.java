cfedd91b70a1c24f8bb419526117a84c89b3161e
shibo <shibo92@qq.com>
2018-11-02 10:02:37 +0800

    delete test model

--- src/test/java/springaop/Test.java
+++
@@ -1,9 +0,0 @@
-package springaop;
-
-public class Test {
-    public static void main(String[] args) {
-        MyProxyManager manager = new MyProxyManager();
-        UserService userService = (UserService) manager.bind(new UserServiceImpl());
-        userService.sayHello("lalala");
-    }
-}
--- src/test/java/springaop/UserService.java
+++
@@ -1,6 +0,0 @@
-package springaop;
-
-public interface UserService {
-    public void sayHello(String word);
-    public void sayHello2(String word);
-}
